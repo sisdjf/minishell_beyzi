d42525287a2bae509719ba969b69a82523edd59a
sisdjf <sirinezl@yahoo.com>
2024-10-17 20:55:27 +0200

    ok

--- srcs/parsing/quotes.c
+++ srcs/parsing/quotes.c
@@ -6,7 +6,7 @@
 /*   By: sizitout <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 23:05:27 by sizitout          #+#    #+#             */
-/*   Updated: 2024/10/15 23:06:24 by sizitout         ###   ########.fr       */
+/*   Updated: 2024/10/16 23:54:46 by sizitout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,4 +37,29 @@ int	ft_quotes(char *str)
 		return (1);
 	}
 	return (0);
+}
+char	*delete_quote(char *str)
+{
+	char	*new;
+	int		i;
+	int		n;
+	int		c;
+
+	new = malloc(sizeof(char) * ft_strlen(str) + 1);
+	i = 0;
+	n = 0;
+	while (str[i])
+	{
+		while (str[i] == '\'' || str[i] == '\"')
+		{
+			c = str[i++];
+			while (str[i] != c)
+				new[n++] = str[i++];
+			i++;
+		}
+		if (str[i])
+			new[n++] = str[i++];
+	}
+	new[i] = '\0';
+	return (new);
 }
