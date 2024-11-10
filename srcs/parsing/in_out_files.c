2bcba4bdc5d789db80e0ffc55c89930fffbd622c
lynda369 <beylynda92@gmail.com>
2024-11-10 23:44:06 +0100

    J ai merge avec dev  et j ai deviser ma fonction infile outfile pour la norm

--- srcs/parsing/in_out_files.c
+++ srcs/parsing/in_out_files.c
@@ -6,12 +6,31 @@
 /*   By: lybey <lybey@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 21:28:56 by lybey             #+#    #+#             */
-/*   Updated: 2024/11/08 19:46:48 by lybey            ###   ########.fr       */
+/*   Updated: 2024/11/10 19:43:19 by lybey            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
+int	stock_infile_norm(t_token *token, t_cmd *new, int i)
+{
+	t_token	*tmp;
+
+	tmp = token;
+	while (tmp && tmp->type != PIPE)
+	{
+		if (tmp->type == REDIR_R)
+		{
+			tmp = tmp->next;
+			new->infile[i] = ft_strdup(tmp->name);
+			printf("new = %s\n", new->infile[i]);
+			i++;
+		}
+		tmp = tmp->next;
+	}
+	new->infile[i] = NULL;
+	return(0);
+}
 int	nbr_malloc_infile_cmd(t_token *token, int pipe)
 {
 	t_token	*tmp;
@@ -62,19 +81,19 @@ int	stock_infile_cmd(t_token *token, int pipe, t_cmd *new)
 			compteur++;
 		tmp = tmp->next;
 	}
-	while (tmp && tmp->type != PIPE)
-	{
-		if (tmp->type == REDIR_L)
-		{
-			tmp = tmp->next;
-			new->infile[i++] = ft_strdup(tmp->name);
-		}
-		tmp = tmp->next;
-	}
-	new->infile[i] = NULL;
+	stock_infile_norm(token, new, i);
+	// while (tmp && tmp->type != PIPE)
+	// {
+	// 	if (tmp->type == REDIR_L)
+	// 	{
+	// 		tmp = tmp->next;
+	// 		new->infile[i++] = ft_strdup(tmp->name);
+	// 	}
+	// 	tmp = tmp->next;
+	// }
+	// new->infile[i] = NULL;
 	return (0);
 }
-
 int	nbr_malloc_outfile_cmd(t_token *token, int pipe)
 {
 	t_token	*tmp;
@@ -102,12 +121,32 @@ int	nbr_malloc_outfile_cmd(t_token *token, int pipe)
 	return (nb_malloc);
 }
 
+int	stock_outfile_norm(t_token *token, t_cmd *new, int i)
+{
+	t_token	*tmp;
+
+	tmp = token;
+	while (tmp && tmp->type != PIPE)
+	{
+		if (tmp->type == REDIR_R)
+		{
+			tmp = tmp->next;
+			new->outfile[i] = ft_strdup(tmp->name);
+			printf("new = %s\n", new->outfile[i]);
+			i++;
+		}
+		tmp = tmp->next;
+	}
+	new->outfile[i] = NULL;
+	return(0);
+}
+
 int	stock_outfile_cmd(t_token *token, int pipe, t_cmd *new)
 {
-	t_token *tmp;
-	int nb_malloc;
-	int compteur;
-	int i;
+	t_token	*tmp;
+	int		nb_malloc;
+	int		compteur;
+	int		i;
 
 	tmp = token;
 	compteur = 0;
@@ -125,17 +164,31 @@ int	stock_outfile_cmd(t_token *token, int pipe, t_cmd *new)
 			compteur++;
 		tmp = tmp->next;
 	}
-	while (tmp && tmp->type != PIPE)
-	{
-		if (tmp->type == REDIR_R)
-		{
-			tmp = tmp->next;
-			new->outfile[i] = ft_strdup(tmp->name);
-			printf("new = %s\n", new->outfile[i]);
-			i++;
-		}
-		tmp = tmp->next;
-	}
-	new->outfile[i] = NULL;
+	stock_outfile_norm(token, new, i);
 	return (0);
 }
+
+// while (tmp && tmp->type != PIPE)
+	// {
+	// 	if (tmp->type == REDIR_R)
+	// 	{
+	// 		tmp = tmp->next;
+	// 		new->outfile[i] = ft_strdup(tmp->name);
+	// 		printf("new = %s\n", new->outfile[i]);
+	// 		i++;
+	// 	}
+	// 	tmp = tmp->next;
+	// }
+	// new->outfile[i] = NULL;
+
+
+	// while (tmp && tmp->type != PIPE)
+	// {
+	// 	if (tmp->type == REDIR_L)
+	// 	{
+	// 		tmp = tmp->next;
+	// 		new->infile[i++] = ft_strdup(tmp->name);
+	// 	}
+	// 	tmp = tmp->next;
+	// }
+	// new->infile[i] = NULL;
