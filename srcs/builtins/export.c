e813c12a848021137c32a5b36a681ad72a2c9880
lynda369 <beylynda92@gmail.com>
2024-11-14 02:47:10 +0100

    dev clean up

--- srcs/builtins/export.c
+++ srcs/builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: lybey <lybey@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 16:58:44 by lybey             #+#    #+#             */
-/*   Updated: 2024/11/14 02:16:39 by lybey            ###   ########.fr       */
+/*   Updated: 2024/11/14 02:46:31 by lybey            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,9 @@ void	print_export(t_envp *envp)
 	while (tmp)
 	{
 		if (!(tmp->value))
-		{
 			printf("export %s\n", tmp->key);
-		}
 		else
-		{
 			printf("export %s=\"%s\"\n", tmp->key, tmp->value);
-		}
 		tmp = tmp->next;
 	}
 }
@@ -71,9 +67,7 @@ int	add_to_env(char *key, char *value, t_envp **envp)
 	while (key[i])
 	{
 		if (ft_isalnum(key[i]) || key[i] == '_')
-		{
 			i++;
-		}
 		else
 			return (printf("bash: export:`%s': not a valid idetifier\n", key),
 					1);
@@ -120,107 +114,3 @@ int	export(char **cmd, t_envp **envp)
 	}
 	return (0);
 }
-// void	print_export(t_envp *envp)
-// {
-// 	t_envp	*tmp;
-
-// 	tmp = envp;
-// 	while (tmp)
-// 	{
-// 		if (!(tmp->value))
-// 			printf("export %s\n", tmp->key);
-// 		else
-// 			printf("export %s=\"%s\"\n", tmp->key, tmp->value);
-// 		tmp = tmp->next;
-// 	}
-// }
-
-// char	*get_key_export(char *str)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	while (str[i])
-// 	{
-// 		if (str[i] == '=')
-// 			return (ft_substr(str, 0, i));
-// 		i++;
-// 	}
-// 	return (strdup(str));
-// }
-// char	*get_value_export(char *str)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	while (str[i])
-// 	{
-// 		if (str[i] == '=')
-// 			return (ft_strdup(str + i + 1));
-// 		i++;
-// 	}
-// 	return (NULL);
-// }
-
-// int	add_to_env(char *key, char *value, t_envp **envp)
-// {
-// 	int		i;
-// 	char	*str;
-// 	char	*mini;
-// 	t_envp	*to_replace;
-
-// 	i = 0;
-// 	// si le premier char de la key est OK
-// 	if (!key[i] || (!ft_isalpha(key[i]) && key[i] != '_'))
-// 		return (printf("bash: export: `%s': not a valid identifier\n", key), 1);
-
-// 	// verifier quil ya pas un char AUTRE que alphanum ou _
-// 	while (key[i])
-// 	{
-// 		if (ft_isalnum(key[i]) || key[i] == '_')
-// 			i++;
-// 		else
-// 			return (printf("bash: export:`%s': not a valid moha\n", key),
-// 					1);
-// 	}
-// 	if (!ft_strchr(key, '='))
-// 		return (0);
-// 	to_replace = search_envp(*envp, key);
-// 	if (to_replace && value)
-// 	{
-// 		free(to_replace->value);
-// 		to_replace->value = value;
-// 	}
-// 	else if (!to_replace)
-// 	{
-// 		str = ft_strjoin(key, "=");
-// 		mini = ft_strjoin(str, value);
-// 		free(value);
-// 		free(str);
-// 		ft_lstadd_back_envp(envp, ft_lstnew_envp(mini));
-// 	}
-// 	return (0);
-// }
-
-// int	export(char **cmd, t_envp **envp)
-// {
-// 	char *key;
-// 	char *value;
-// 	int i;
-
-// 	i = 1;
-// 	while (cmd[i])
-// 	{
-// 		printf("i: %d\n", i);
-// 		key = get_key_export(cmd[i]);
-// 		value = get_value_export(cmd[i]);
-// 		printf("key = %s value = %s\n", key, value);
-// 		add_to_env(key, value, envp);
-// 		free(key);
-// 		i++;
-// 	}
-// 	return (0);
-// }
-
-//export A==a
-//unset A apres ca aussi des pb surement lie au ==
